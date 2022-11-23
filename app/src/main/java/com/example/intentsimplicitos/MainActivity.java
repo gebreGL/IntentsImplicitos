9b701afad9efc053e5a1996673ae1fe03523a602
gebreGL <105040707+gebreGL@users.noreply.github.com>
2022-11-23 12:45:41 +0100

    Initial commit

---
+++ app/src/main/java/com/example/intentsimplicitos/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.intentsimplicitos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onCLickBtn(View view) {
+
+        Intent i = new Intent();
+        switch (view.getId()) {
+            case R.id.btnContactos:
+                i.setAction(Intent.ACTION_VIEW);
+                i.setData(Uri.parse("content://contacts/people/"));
+                startActivity(i);
+                break;
+            case R.id.btnDial:
+                i.setAction(Intent.ACTION_DIAL);
+                startActivity(i);
+                break;
+            case R.id.btnMarcarNumero:
+                i.setAction(Intent.ACTION_DIAL);
+                i.setData(Uri.parse("tel:(+34)677157448"));
+                startActivity(i);
+            case R.id.btnNavegador:
+                i.setAction(Intent.ACTION_VIEW);
+                i.setData(Uri.parse("http://www.edu.xunta.gal/portal"));
+                startActivity(i);
+                break;
+        }
+
+    }
+}
